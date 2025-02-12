e609fc63473386eb4b6b1375d8ec01b7b85c8f0f
GuiTav <guitavarescar@gmail.com>
2025-02-12 00:04:16 -0300

    tst chamada

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,6 +1,20 @@
+<script setup>
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+const data = ref([]);
+
+onMounted(async () => {
+  const resp = await axios.get("http://hello-developer-sandbox-andrepaulinobtw-dev.apps.rm1.0a51.p1.openshiftapps.com/movies");
+  data.value = resp.data;
+})
+
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p v-for="item in data">{{ item.title }}</p>
   </div>
 </template>
 
