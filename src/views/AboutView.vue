5b04b87681bb3a94a61d0f0fcdfd2c7dbf17bd78
André Paulino <andrepaulinobtw@gmail.com>
2025-02-11 23:30:31 -0300

    ref: remove codigo alem do template

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,32 +1,6 @@
-<script>
-import axios from 'axios';
-
-export default {
-  data() {
-    return {
-      items: []
-    };
-  },
-  created() {
-    this.fetchData();
-  },
-  methods: {
-    async fetchData() {
-      try {
-        const response = await axios.get("http://hello-developer-sandbox-andrepaulinobtw-dev.apps.rm1.0a51.p1.openshiftapps.com/movies");
-        this.items = response.data;
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    }
-  }
-};
-</script>
-
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <p v-for="item in items" :key="item.id">{{ item.title }}</p>
   </div>
 </template>
 
